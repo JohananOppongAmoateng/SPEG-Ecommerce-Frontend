303ee4abe2ac819bff6377ac83d9bb732198586a
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2025-07-30 02:48:51 +0000

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,10 +4,14 @@ import { Navigate, useLocation } from "react-router-dom";
 import LogoLoader from "./LogoLoader";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useContext(AuthContext);
+  const { isAuthenticated, loading, user } = useContext(AuthContext);
   const location = useLocation();
 
   if (loading) return <LogoLoader />;
+
+  if (!user || user.emailVerified && !user.adminVerified) {
+    <Navigate to="/login" state={{ from: location.pathname }} replace />
+  }
 
   return isAuthenticated ? (
     children
